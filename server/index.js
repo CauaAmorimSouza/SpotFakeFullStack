69985c0bb9e119b5f1e76c4461aad72fb3dd47da
Cauã Souza <caua_a_souza@estudante.sesisenai.org.br>
2024-10-22 12:39:12 -0300

    update

--- server/index.js
+++ server/index.js
@@ -1,16 +1,31 @@
 import Express from "express";
+import { criarTabelas, User } from "./db.js";
 
 const app = Express()
 app.use(Express.json())
+//criarTabelas()
 
-app.post('/registro', (req, res) => {
+app.post('/registro', async (req, res) => {
     const {nome, sobrenome, email, senha, dataNascimento} = req.body
+
+    if(!nome || !sobrenome || !email || !senha || !dataNascimento){
+        res.send('você deve preecher todos os campos')
+        return
+    }
+    const teste = await User.create({nome, sobrenome, email, senha, dataNascimento})
     console.log(email)
-    res.send('tá funfando tbm')
+    res.send('usuário criado')
+})
+
+app.post('/login', (req, res) => {
+    const {email, senha} = req.body
+
+    if(!email || !senha ){
+        res.send('você deve preecher todos os campos')
+        return
+    }
+    console.log(email)
+    res.send('usuário criado')
 })
 
 app.listen(8000)
-/*/app.get('/registro', (req, res) => {
-    console.log('tá funfando')
-    res.send('tá funfando tbm')
-})/*/
